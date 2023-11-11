bbed9283c3af746735a9aa384d04c9f5e6c12db7
00JCIV00 <jake.crawfordiv@gmail.com>
2023-11-10 21:48:55 -0500

    Implemented Multi-Threaded Interactions
    - Implemented multithreading for the Reaction Functions used during Datagram Interactions.

--- src/interact.zig
+++ src/interact.zig
@@ -19,19 +19,17 @@ const Addresses = lib.Addresses;
 const Datagrams = lib.Datagrams;
 
 /// Interaction Config.
-/// **Comptime Only!**
 pub const InteractConfig = struct{
-    /// Receive Buffer Max size. (Max 4096)
-    recv_buf_max: u13 = 4096,
-
-    /// Max number of Datagrams to receive one Batch of CQEs.
-    /// TODO: Figure out if IO_Uring can be used for AF_PACKET, SOCK_RAW, ETH_P_ALL sockets.
-    recv_batch_max: u32 = 1,
-
     /// Max number of Datagrams to be processed.
     /// Setting this to 0 will allow for infinite loops
     recv_dgs_max: u32 = 10,
+    /// Run Reaction Functions in their own Thread.
+    multithreaded: bool = true,
+};
 
+/// Interaction Functions.
+/// **Comptime Only!**
+pub const InteractFunctions = struct{
     /// A Function to be called at the Start of an Interaction.
     ///
     /// Parameters:
@@ -53,40 +51,31 @@ pub const InteractConfig = struct{
     /// 1. Allocator
     /// 2. Context
     end_fn: ?*const fn(mem.Allocator) anyerror!void = null,
-
-    // A Context object to be used in Function calls.
-    //fn_ctx: ?*anyopaque = null,
 };
 
 /// Interact with a Network using the provided Allocator (`alloc`), Function Context (`fn_ctx`), and Interaction Config (`config`).
-pub fn interact(alloc: mem.Allocator, fn_ctx: anytype, sock_config: conn.IFSocket.IFSocketInitConfig, comptime ia_config: InteractConfig) !void {
+pub fn interact(
+    alloc: mem.Allocator, 
+    fn_ctx: anytype, 
+    sock_config: conn.IFSocket.IFSocketInitConfig, 
+    ia_config: InteractConfig, 
+    comptime ia_fns: InteractFunctions
+) !void {
     // Setup Sockets
     var recv_sock = try conn.IFSocket.init(sock_config);
     defer recv_sock.close();
 
-    // Setup IO_Uring
-    var recv_io = try os.linux.IO_Uring.init(ia_config.recv_buf_max, 0);
-    defer recv_io.deinit();
-    var recv_cqes = try alloc.alloc(os.linux.io_uring_cqe, ia_config.recv_buf_max);
-    defer alloc.free(recv_cqes);
-
-    // Run the Start Function (if applicable)
-    if (ia_config.start_fn) |startFn| try startFn(alloc, fn_ctx);
-
     // Setup for Receiving Datagrams
-    //var recv_buf: [4096]u8 = .{ 0 } ** 4096;
-    //var recv_buf = std.RingBuffer.init(alloc, 4096);
+    const buf_size = 4096;
     var recv_buf = try alloc.alloc(
-        [ia_config.recv_buf_max]u8, 
+        [buf_size]u8, 
         if (ia_config.recv_dgs_max > 0) ia_config.recv_dgs_max
         else 100,
     );
     defer alloc.free(recv_buf);
 
-    //const l2_type: meta.Tag(Datagrams.Layer2Header) = switch (recv_sock.hw_fam) {
-    //    consts.ARPHRD_ETHER => .eth,
-    //    else => return error.UnimplementedType,
-    //};
+    // Run the Start Function (if applicable)
+    if (ia_fns.start_fn) |startFn| try startFn(alloc, fn_ctx);
 
     // Receive Datagrams and React to them (if applicable)
     var dg_count: u32 = 0;
@@ -98,7 +87,7 @@ pub fn interact(alloc: mem.Allocator, fn_ctx: anytype, sock_config: conn.IFSocke
     ) : ({
         if (infinite_dgs and dg_idx == 99) {
             alloc.free(recv_buf);
-            recv_buf = try alloc.alloc([ia_config.recv_buf_max]u8, 100);
+            recv_buf = try alloc.alloc([buf_size]u8, 100);
         }
         dg_count += 1;
         dg_idx = if (infinite_dgs) dg_count % 100 else dg_count;
@@ -107,49 +96,22 @@ pub fn interact(alloc: mem.Allocator, fn_ctx: anytype, sock_config: conn.IFSocke
             error.UnimplementedType => continue,
             else => return err,
         };
-        if (ia_config.react_fn) |reactFn| try reactFn(alloc, fn_ctx, datagram);
+        if (ia_fns.react_fn) |reactFn| {
+            if (!ia_config.multithreaded) try reactFn(alloc, fn_ctx, datagram)
+            else {
+                log.debug("Spawning Thread.", .{});
+                //thread_pool.spawn(reactFn, .{ alloc, fn_ctx, datagram });
+                var thread = try std.Thread.spawn(
+                    .{ .allocator = alloc },
+                    reactFn.*,
+                    .{ alloc, fn_ctx, datagram }
+                );
+                thread.join();
+            }
+        }
     }
 
-    // TODO: Figure out IO_Uring support.
-    // Receive Datagrams and React to them (if applicable)
-    //var cqe_count: u32 = 0;
-    //while (
-    //    if (ia_config.recv_dgs_max > 0) cqe_count < ia_config.recv_dgs_max
-    //    else true
-    //) {
-    //    //os.linux.io_uring_prep_recv(try recv_io.get_sqe(), recv_sock.desc, recv_buf[0..1518], 0);
-    //    _ = try recv_io.recv(0, recv_sock.desc, .{ .buffer = recv_buf[0..] }, 0);
-    //    const sqe_submit = try recv_io.submit_and_wait(1);//ia_config.recv_batch_max);
-    //    log.debug("SQEs Submitted: {d}.", .{ sqe_submit });
-
-    //    if (recv_io.cq_ready() == 0) continue;
-
-
-    //    const num_cqes = try recv_io.copy_cqes(recv_cqes, ia_config.recv_batch_max);
-    //    log.debug("Processing {d} CQE(s)...", .{ num_cqes });
-    //    for (recv_cqes[0..num_cqes], 0..) |cqe, idx| {
-    //        log.debug("- Checking CQE #{d}...", .{ idx });
-    //      if (cqe.res == 0) {
-    //          log.debug("- No bytes received.", .{}); 
-    //          continue;
-    //      }
-    //      if (cqe.res < 0) {
-    //          const errno = cqe.res * -1;
-    //          log.err("Error receiving bytes: #{d} - {s}\n{any}", .{ 
-    //              errno, 
-    //              @tagName(@as(os.linux.E, @enumFromInt(errno))),
-    //              cqe
-    //          });
-    //          return error.ReceiveError;
-    //      }
-    //      log.debug("Received {d}B Datagram.", .{ cqe.res });
-    //      const datagram = try Datagrams.Full.fromBytes(alloc, recv_buf[0..], l2_type);
-    //      if (ia_config.react_fn) |reactFn| try reactFn(alloc, fn_ctx, datagram);
-    //    }
-    //    log.debug("Processed {d} CQE(s).", .{ num_cqes });
-
-    //}
 
     // Run the End Function (if applicable)
-    if (ia_config.end_fn) |endFn| try endFn(alloc, fn_ctx);
+    if (ia_fns.end_fn) |endFn| try endFn(alloc, fn_ctx);
 }
--- src/tools/record.zig
+++ src/tools/record.zig
@@ -18,23 +18,23 @@ pub const RecordConfig = struct{
     /// Filename.
     filename: ?[]const u8 = null,
     /// Enable Printing of Datagrams to `stdout`.
-    enable_print: ?bool = true,
+    print: ?bool = true,
     /// Datagram Separator.
     dg_sep: ?[]const u8 = "===============================================",
-    /// Ring Buffer Max size (Max: 4096)
-    ring_buf_max: ?u13 = 4096,
     /// Interface Name.
     if_name: ?[]const u8 = "eth0",
+    /// Receive Datagrams Max.
+    recv_dgs_max: ?u32 = 0,
+    /// Enable Multi-Threading.
+    multithreaded: ?bool = true,
 };
 
 /// Record Context.
 const RecordContext = struct{
-    /// Record Config.
-    record_config: RecordConfig = .{},
-    /// Record IO_Uring
-    record_io: *os.linux.IO_Uring,
-    /// Record Completion Query Events
-    record_cqes: []os.linux.io_uring_cqe,
+    /// Enable Printing of Datagrams to `stdout`.
+    enable_print: bool = true,
+    /// Datagram Separator.
+    dg_sep: []const u8 = "===============================================",
     /// Record File
     record_file: *?fs.File,
     /// Datagrams Count.
@@ -43,59 +43,56 @@ const RecordContext = struct{
 
 /// Record Datagrams.
 pub fn record(alloc: mem.Allocator, config: RecordConfig) !void {
-    const ring_buf_max = config.ring_buf_max.?;
-    var record_io = try os.linux.IO_Uring.init(ring_buf_max, 0);
-    defer record_io.deinit();
-    var record_cqes = try alloc.alloc(os.linux.io_uring_cqe, ring_buf_max);
-    defer alloc.free(record_cqes);
-
     var cwd = fs.cwd();
     var record_file = 
         if (config.filename) |filename| try cwd.createFile(filename, .{ .truncate = false })
         else null;
     defer if (record_file) |file| file.close();
 
+    var record_ctx = RecordContext{
+        .enable_print = config.print.?,
+        .dg_sep = config.dg_sep.?,
+        .record_file = &record_file,
+    };
+
     try ia.interact(
         alloc, 
-        RecordContext{
-            .record_config = config,
-            .record_io = &record_io,
-            .record_cqes = record_cqes,
-            .record_file = &record_file,
-        },
+        &record_ctx,
         .{ .if_name = config.if_name.? },
+        .{
+            .recv_dgs_max = config.recv_dgs_max.?,
+            .multithreaded = config.multithreaded.?,
+        },
         .{ .react_fn = recordReact },
     );
 }
 
 /// Record Reaction Function.
 fn recordReact(_: mem.Allocator, ctx: anytype, datagram: Datagrams.Full) !void {
-    if (@TypeOf(ctx) != RecordContext) @compileError("This Reaction Function requires a context of Type `RecordCtx`.");
-    //var ctx = ctx orelse {
-    //    log.err("This Reaction Function requires a context of Type `RecordCtx`.", .{});
-    //    return error.NullContextProvided;
-    //};
+    if (@TypeOf(ctx) != *RecordContext) @compileError("This Reaction Function requires a context of Type `RecordCtx`.");
     
     const text_fmt = 
        \\
        \\ {d}:
        \\ {s}
        \\
-       \\ {?s}
+       \\ {s}
        \\
     ;
     const text_ctx = .{
         time.timestamp(),
         datagram,
-        ctx.record_config.dg_sep,
+        ctx.dg_sep,
     };
 
-    // TODO: Redo this with IO_Uring 
+    // TODO: Redo this with IO_Uring? 
     if (ctx.record_file.*) |file| {
         try file.seekFromEnd(0);
         try file.writer().print(text_fmt, text_ctx);
     }
 
-    if (ctx.record_config.enable_print orelse true) try io.getStdOut().writer().print(text_fmt, text_ctx);
+    if (ctx.enable_print) try io.getStdOut().writer().print(text_fmt, text_ctx);
     log.debug("Recorded Datagram.", .{});
+
+    ctx.*.count += 1;
 }
