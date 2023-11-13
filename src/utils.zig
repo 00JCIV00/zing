4e4443b07e7831b574364a0841b0dd6f14267f8b
00JCIV00 <jake.crawfordiv@gmail.com>
2023-11-13 00:22:50 -0500

    Added `MergedStruct()` to `utils.zig`
    - Added `MergedStruct()` to allow 2 or more Structs with no Declarations to be merged.

--- src/utils.zig
+++ src/utils.zig
@@ -1,6 +1,8 @@
 //! Utility functions for the Zing Library
 
 const std = @import("std");
+const ascii = std.ascii;
+const fmt = std.fmt;
 const mem = std.mem;
 const meta = std.meta;
 
@@ -84,4 +86,40 @@ pub fn Iterator(comptime ChildT: type) type {
             };
         }
     };
+}
+
+/// Merge Fields from all given Structs using comptime reification.
+/// Note this only works for Structs without Declarations.
+pub fn MergedStruct(comptime types: []const type) type {
+    var base_info = @typeInfo(types[0]);
+    if (base_info != .Struct) @compileError("All Merged Meta Types must be Structs!");
+    for (types[1..]) |AddT| {
+        if (@typeInfo(AddT) != .Struct) @compileError("All Merged Meta Types must be Structs!");
+        base_info.Struct.fields = base_info.Struct.fields ++ meta.fields(AddT);
+    }
+    return @Type(.{ .Struct = base_info.Struct });
+}
+
+
+/// Find the Index of any Type, Scalar or Slice, (`needle`) within a Slice of that Type (`haystack`). (Why is this not in std.mem?!?!? Did I miss it?)
+/// (Borrowed from Cova. Make a Utils Library?)
+pub fn indexOfEql(comptime T: type, haystack: []const T, needle: T) ?usize {
+    switch (@typeInfo(T)) {
+        .Pointer => |ptr| {
+            for (haystack, 0..) |hay, idx| if (mem.eql(ptr.child, hay, needle)) return idx;
+            return null;
+        },
+        .Struct => {
+            for (haystack, 0..) |hay, idx| if (meta.eql(hay, needle)) return idx;
+            return null;
+        },
+        inline else => return mem.indexOfScalar(T, haystack, needle),
+    }
+}
+
+/// Find the Index of a String (`needle`) within a Slice of Strings `haystack`. (Why is this not in std.mem?!?!? Did I miss it?)
+/// (Borrowed from Cova. Make a Utils Library?)
+pub fn indexOfEqlIgnoreCase(haystack: []const []const u8, needle: []const u8) ?usize {
+    for (haystack, 0..) |hay, idx| if (ascii.eqlIgnoreCase(hay, needle)) return idx;
+    return null;
 }
