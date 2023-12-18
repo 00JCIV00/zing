6bce354519208d03891eb900721d5c9051f9ed85
00JCIV00 <jake.crawfordiv@gmail.com>
2023-12-18 14:57:48 -0500

    Started `filter.zig`
    - Started `filter.zig` to handle eBPF based datagram filtering.

--- src/filter.zig
+++ src/filter.zig
@@ -13,16 +13,29 @@ const process = std.process;
 const time = std.time;
 
 const lib = @import("zinglib.zig");
+const Addresses = lib.Addresses;
 
+
+const load_ether = BPF.Insn{
+    .code = 0x28,
+    .dst = 0,
+    .src = 0,
+    .off = 12,
+    .imm = 0x0000000c,
+};
 
 /// An abstraction over BPF Filters
 pub const Filter = struct{
     /// File descriptor of this Filter's Map.
     map: os.linux.fd_t,
 
+    /// Base Instructions 
+    bpf_instructions: []BPF.Insn = &.{}, 
 
     /// Initialize this Filter
     pub fn init() !@This() {
+        const ip = try Addresses.IPv4.fromStr("192.168.0.1");
+        const port: u32 = 80;
         const map = try BPF.map_create(.hash, 4, 8, 2);
         try BPF.map_update_elem(map, "6", "0", 0);
 
@@ -33,6 +46,19 @@ pub const Filter = struct{
         //    
         //    BPF.Insn.exit(),
         //};
+        const prog: []BPF.Insn = &.{
+            BPF.Insn{ 0x28, 0, 0, 12, 0x0000000c },
+            BPF.Insn{ 0x15, 0, 1, 0, 0x00000006 },
+            BPF.Insn{ 0x20, 0, 0, 12, @bitCast(ip) },
+            BPF.Insn{ 0x20, 0, 0, 0, 0x0000000e },
+            BPF.Insn{ 0x15, 0, 3, 0, port },
+
+            BPF.Insn{ 0x6, 0, 0, 0, 0xFFFF },
+            BPF.Insn{ 0x6, 0, 0, 0, 0x0 },
+            
+            BPF.Insn.exit(),
+        };
+        _ = prog;
 
 
 
